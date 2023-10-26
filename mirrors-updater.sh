86c299ae0cf0d48c06084a642eda6a520cad268a
Pingu <130224668+pingu0b@users.noreply.github.com>
2023-10-26 23:55:19 +0530

    Update mirrors-updater.sh

--- mirrors-updater.sh
+++ mirrors-updater.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-cd /tmp/
-git clone https://github.com/pingu0b/pacman-mirrorlist-in.git
-cd pacman-mirrorlist-in
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bkp
 cat pacman-mirrorlist-in > /etc/pacman.d/mirrorlist
 echo "######## Your mirrors have been succesfully updated ########"
